5388c46c01cc59b298f81004711288d874a60d08
sunnysesh <su.seshadri@googlemail.com>
2024-06-08 11:30:43 +0100

    Asset service skeleton

---
+++ src/PlatformOneAsset.Core/Services/AssetService.cs
@@ -0,0 +1,28 @@
+﻿using PlatformOneAsset.Core.Interfaces;
+using PlatformOneAsset.Core.Models.Request;
+using PlatformOneAsset.Core.Models.Response;
+
+namespace PlatformOneAsset.Core.Services;
+
+public class AssetService : IAssetService
+{
+    public Task<IEnumerable<AssetResponse>> GetAllAssetsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<AssetResponse> GetAssetViaSymbolAsync(string symbol)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<AssetResponse> AddAssetAsync(CreateAssetRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<AssetResponse> UpdateAssetAsync(string symbol, UpdateAssetRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}

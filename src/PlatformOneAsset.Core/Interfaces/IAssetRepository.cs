3a07d45a86b6b1cdfcdf4aa83c73407c47d6447e
sunnysesh <su.seshadri@googlemail.com>
2024-06-08 11:16:38 +0100

    Asset interfaces

---
+++ src/PlatformOneAsset.Core/Interfaces/IAssetRepository.cs
@@ -0,0 +1,11 @@
+﻿using PlatformOneAsset.Core.Models.Entities;
+
+namespace PlatformOneAsset.Core.Interfaces;
+
+public interface IAssetRepository
+{
+    IEnumerable<Asset> GetAll();
+    Asset GetByReference(string reference);
+    Asset Add(Asset asset);
+    Asset Update(Asset asset);
+}
---
+++ src/PlatformOneAsset.Core/Interfaces/IAssetService.cs
@@ -0,0 +1,15 @@
+﻿using PlatformOneAsset.Core.Models.Request;
+using PlatformOneAsset.Core.Models.Response;
+
+namespace PlatformOneAsset.Core.Interfaces;
+
+public interface IAssetService
+{
+    Task<IEnumerable<AssetResponse>> GetAllAssetsAsync();
+
+    Task<AssetResponse> GetAssetViaSymbolAsync(string symbol);
+
+    Task<AssetResponse> AddAssetAsync(CreateAssetRequest request);
+
+    Task<AssetResponse> UpdateAssetAsync(string symbol, UpdateAssetRequest request);
+}

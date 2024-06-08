4b5b36cf7912338700ac1b5f145e0895ebd9ca5a
sunnysesh <su.seshadri@googlemail.com>
2024-06-08 18:17:36 +0100

    Get all or specified asset(s) endpoints

--- src/PlatformOneAsset.App/Program.cs
+++ src/PlatformOneAsset.App/Program.cs
@@ -1,7 +1,16 @@
+using PlatformOneAsset.Core.Interfaces;
+using PlatformOneAsset.Core.Profiles;
+using PlatformOneAsset.Core.Repositories;
+using PlatformOneAsset.Core.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+builder.Services.AddScoped<IAssetService, AssetService>();
+builder.Services.AddSingleton<IAssetRepository, AssetRepository>();
 
 var app = builder.Build();
 
@@ -10,6 +19,21 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapGet("/assets", async (IAssetService assetService) =>
+{
+    var assetResponses = await assetService.GetAllAssetsAsync();
+    return Results.Ok(assetResponses);
+});
+
+app.MapGet("/assets/{symbol}", async (string symbol, IAssetService assetService) =>
+{
+    var response = await assetService.GetAssetViaSymbolAsync(symbol);
+    if (response == null)
+        return Results.NotFound();
+
+    return Results.Ok(response);
+});
 
 app.UseHttpsRedirection();
 

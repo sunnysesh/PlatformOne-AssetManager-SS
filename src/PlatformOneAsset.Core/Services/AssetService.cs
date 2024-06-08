f7963469a5290ec9159b22ec3ad3a300d5a76e50
sunnysesh <su.seshadri@googlemail.com>
2024-06-08 17:52:00 +0100

    Get asset service implementation

--- src/PlatformOneAsset.Core/Services/AssetService.cs
+++ src/PlatformOneAsset.Core/Services/AssetService.cs
@@ -1,4 +1,5 @@
-﻿using PlatformOneAsset.Core.Interfaces;
+﻿using AutoMapper;
+using PlatformOneAsset.Core.Interfaces;
 using PlatformOneAsset.Core.Models.Request;
 using PlatformOneAsset.Core.Models.Response;
 
@@ -6,22 +7,35 @@ namespace PlatformOneAsset.Core.Services;
 
 public class AssetService : IAssetService
 {
-    public Task<IEnumerable<AssetResponse>> GetAllAssetsAsync()
+    private readonly IAssetRepository _assetRepository;
+    private readonly IMapper _mapper;
+
+    public AssetService(IAssetRepository assetRepository, IMapper mapper)
+    {
+        _assetRepository = assetRepository;
+        _mapper = mapper;
+    }
+    
+    public async Task<IEnumerable<AssetResponse>> GetAllAssetsAsync()
+    {
+        var results = _assetRepository.GetAll();
+        return _mapper.Map<IEnumerable<AssetResponse>>(results);
+    }
+
+    public async Task<AssetResponse> GetAssetViaSymbolAsync(string symbol)
+    {
+        var result = _assetRepository.GetBySymbol(symbol);
+        return result is null
+            ? null
+            : _mapper.Map<AssetResponse>(result);
+    }
+
+    public async Task<AssetResponse> AddAssetAsync(CreateAssetRequest request)
     {
         throw new NotImplementedException();
     }
 
-    public Task<AssetResponse> GetAssetViaSymbolAsync(string symbol)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<AssetResponse> AddAssetAsync(CreateAssetRequest request)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<AssetResponse> UpdateAssetAsync(string symbol, UpdateAssetRequest request)
+    public async Task<AssetResponse> UpdateAssetAsync(string symbol, UpdateAssetRequest request)
     {
         throw new NotImplementedException();
     }

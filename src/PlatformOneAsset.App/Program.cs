35a932ba36762347cf6767e6533e0ced535f17e2
sunnysesh <su.seshadri@googlemail.com>
2024-06-09 12:40:10 +0100

    Add asset validation

--- src/PlatformOneAsset.App/Program.cs
+++ src/PlatformOneAsset.App/Program.cs
@@ -1,4 +1,8 @@
+using FluentValidation;
+using PlatformOneAsset.App.Validators;
+using PlatformOneAsset.Core.Exceptions;
 using PlatformOneAsset.Core.Interfaces;
+using PlatformOneAsset.Core.Models.Request;
 using PlatformOneAsset.Core.Profiles;
 using PlatformOneAsset.Core.Repositories;
 using PlatformOneAsset.Core.Services;
@@ -8,6 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
 builder.Services.AddScoped<IAssetService, AssetService>();
 builder.Services.AddSingleton<IAssetRepository, AssetRepository>();
@@ -33,6 +39,33 @@ app.MapGet("/assets/{symbol}", async (string symbol, IAssetService assetService)
         return Results.NotFound();
 
     return Results.Ok(response);
+});
+
+app.MapPost("/assets", async (CreateAssetRequest request ,IAssetService assetService, IValidator<CreateAssetRequest> validator) =>
+{
+    var validResult = await validator.ValidateAsync(request);
+    if (!validResult.IsValid)
+        return Results.BadRequest($"Validation error occured. {validResult.ToString()}");
+    
+    try
+    {
+        var response = await assetService.AddAssetAsync(request);
+        return Results.Created($"/assets/{response.Symbol}", response);
+    }
+    catch (EntityAlreadyExistsException ex)
+    {
+        return Results.Conflict(new
+        {
+            message = ex.Message
+        });
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(
+            detail: ex.Message,
+            statusCode: StatusCodes.Status500InternalServerError
+        );
+    }
 });
 
 app.UseHttpsRedirection();
---
+++ src/PlatformOneAsset.App/Validators/CreateAssetRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using PlatformOneAsset.Core.Models.Request;
+
+namespace PlatformOneAsset.App.Validators;
+
+public class CreateAssetRequestValidator : AbstractValidator<CreateAssetRequest>
+{
+    public CreateAssetRequestValidator()
+    {
+        RuleFor(i => i.Name).NotEmpty();
+        RuleFor(i => i.Symbol).NotEmpty();
+        RuleFor(i => i.ISIN).NotEmpty();
+    }
+}

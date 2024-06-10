e3f27a4ff5f57d67ac473135e59f136b3cf7e9d1
sunnysesh <su.seshadri@googlemail.com>
2024-06-10 08:02:34 +0100

    Price entity

---
+++ src/PlatformOneAsset.Core/Models/Entities/Price.cs
@@ -0,0 +1,12 @@
+﻿namespace PlatformOneAsset.Core.Models.Entities;
+
+public class Price
+{
+    public string Symbol { get; set; }
+    public DateTime Date { get; set; }
+    public decimal Value { get; set; }
+    public DateTime TimeStamp { get; set; }
+
+    public void UpdateTimeStamp(DateTime timestamp)
+        => TimeStamp = timestamp;
+}

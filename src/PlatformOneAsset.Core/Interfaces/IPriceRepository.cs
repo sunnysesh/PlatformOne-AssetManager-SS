c5d1536d8780dae9ac6b64cd3180e9a87c39cd91
sunnysesh <su.seshadri@googlemail.com>
2024-06-10 08:02:47 +0100

    Price repository skeleton

---
+++ src/PlatformOneAsset.Core/Interfaces/IPriceRepository.cs
@@ -0,0 +1,8 @@
+﻿using PlatformOneAsset.Core.Models.Entities;
+
+namespace PlatformOneAsset.Core.Interfaces;
+
+public interface IPriceRepository : IRepository<Price>
+{
+    IEnumerable<Price> GetPrices(string symbol, DateTime date, string source = null);
+}
---
+++ test/PlatformOneAsset.UnitTests/Repositories/PriceRepositoryUnitTests.cs
@@ -0,0 +1,69 @@
+﻿using FluentAssertions;
+using PlatformOneAsset.Core.Models.Entities;
+
+namespace PlatformOneAsset.UnitTests.Repositories;
+
+public class PriceRepositoryUnitTests
+{
+    private PriceRepository _priceRepository;
+
+    [SetUp]
+    public void Setup()
+    {
+        _priceRepository = new PriceRepository();
+    }
+
+    [Test]
+    public void GetByDate_WhenPricesExistForDate_ShouldReturnCorrectPrices()
+    {
+        //Arrange
+        string symbol = "MSFT";
+        var expectedPrices = new List<Price>()
+        {
+            new Price()
+            {
+                Symbol = symbol,
+                Date = DateTime.Today,
+                Value = 50.0m
+            },
+            new Price()
+            {
+                Symbol = symbol,
+                Date = DateTime.Today.AddDays(1),
+                Value = 30.0m
+            },
+        };
+        _priceRepository.Add(expectedPrices[0]);
+        _priceRepository.Add(expectedPrices[1]);
+
+        //Act
+        var result = _priceRepository.GetPrices(symbol, DateTime.Today);
+
+        //Assert
+        result.Should().HaveCount(1);
+        result[0].Should().BeEquivalentTo(expectedPrices);
+    }
+    
+    [Test]
+    public void GetByDate_WhenPricesDontExistForDate_ShouldReturnEmptyCollection()
+    {
+        //Arrange
+        string symbol = "MSFT";
+        var expectedPrices = new List<Price>()
+        {
+            new Price()
+            {
+                Symbol = symbol,
+                Date = DateTime.Today.AddDays(1),
+                Value = 30.0m
+            },
+        };
+        _priceRepository.Add(expectedPrices[0]);
+        
+        //Act
+        var result = _priceRepository.GetPrices(symbol, DateTime.Today);
+        
+        //Assert
+        result.Should().BeEmpty();
+    }
+}
